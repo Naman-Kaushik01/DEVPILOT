92fd3a3b9ca0e17c3c51b3d49ab883342b60bb50
Naman Kaushik <kaushiknaman68@gmail.com>
2026-08-30 00:10:08 +0530

    Implemented UserEntity

--- backend/src/main/java/devPilot/backend/entity/User.java
+++ backend/src/main/java/devPilot/backend/entity/User.java
@@ -1,19 +1,49 @@
 package devPilot.backend.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.time.Instant;
 import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="users")
+@Builder
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(name ="github_username" ,nullable = false, length = 25 )
     private Long githubId;
 
+    @Column(name ="github_user_name" ,nullable = false, length = 200 )
     private String githubUserName;
 
+    @Column(name ="display_name" ,nullable = false, length = 200 )
     private String displayName;
 
+    @Column(name ="avatar_url" ,nullable = true, length = 500 )
     private String avatarUrl;
+
+    @Column(name="access_token" ,nullable = false, columnDefinition = "TEXT")
+    private String accessToken;
+
+    @Column(name ="token_scopes" , length = 500)
+    private String tokenScopes;
+
+    @Column(name ="created_at" , nullable = false , updatable = false )
+    private Instant createdAt;
+
+    @PrePersist
+    void onCreate() {
+        if (createdAt == null) {
+            createdAt = Instant.now();
+        }
+    }
+
 }
