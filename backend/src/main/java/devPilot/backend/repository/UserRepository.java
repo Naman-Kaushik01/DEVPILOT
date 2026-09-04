bc31ec84e8fa4f5573afc59cec96682b7daf41b3
Naman Kaushik <kaushiknaman68@gmail.com>
2026-09-04 12:22:50 +0530

    fixed Imports

--- backend/src/main/java/devPilot/backend/repository/UserRepository.java
+++ backend/src/main/java/devPilot/backend/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package devPilot.backend.repository;
 
 import devPilot.backend.entity.User;
-import org.hibernate.internal.util.Optional;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
