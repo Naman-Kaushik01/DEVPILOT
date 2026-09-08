8e7fc49df701430c00bcfc2a02f9ff6805713aaa
Naman Kaushik <kaushiknaman68@gmail.com>
2026-09-08 21:11:23 +0530

    created User-Response DTO

---
+++ backend/src/main/java/devPilot/backend/dtos/UserResponse.java
@@ -0,0 +1,14 @@
+package devPilot.backend.dtos;
+
+import java.util.UUID;
+
+public record UserResponse(
+        UUID id,
+        Long githubId,
+        String githubUsername,
+        String displayName,
+        String avatarUrl
+) {
+
+
+}
