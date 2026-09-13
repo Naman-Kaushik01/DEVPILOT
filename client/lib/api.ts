205b6f0fb386ce2cac44b4261099bc4bda4b2d0a
Naman Kaushik <kaushiknaman68@gmail.com>
2026-09-13 20:46:21 +0530

    created functions

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -5,3 +5,11 @@ export type User = {
   displayName: string;
   avatarUrl: string | null;
 };
+
+export function getApiBaseUrl() {
+  return process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080";
+}
+
+export function getGithubLoginUrl() {
+  return `${getApiBaseUrl()}/oauth2/authorization/github`;
+}
