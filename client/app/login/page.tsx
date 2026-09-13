153bf08af2e98c7ce460c12a6c171953af611c9b
Naman Kaushik <kaushiknaman68@gmail.com>
2026-09-13 21:03:08 +0530

    created devpilot-icon

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -19,7 +19,7 @@ import {
 } from "@/components/ui/card";
 import { Spinner } from "@/components/ui/spinner";
 import { cn } from "@/lib/utils";
-import { getGithubLoginUrl } from '@/lib/api';
+import { getGithubLoginUrl } from "@/lib/api";
 import { useCurrentUser } from '@/hooks/use-auth';
 
 function LoginLoading(){
